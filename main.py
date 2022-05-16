44322a4abda587a11b25851bc45a43136c612fa1
jojo <joshboys666@gmail.com>
2022-05-16 13:08:20 -0300

    test1

---
+++ main.py
@@ -0,0 +1,46 @@
+import pyautogui
+import pyperclip
+import time
+
+pyautogui.PAUSE = 0.5
+
+while True:
+    #ABRIR COREL
+    pyautogui.hotkey("win", "e")
+    pyautogui.click(x=540, y=101)
+    pyautogui.hotkey("ctrl", "a")
+    pyautogui.press("backspace")
+    pyperclip.copy(r"A:\videos\PACK 500 ESTAMPAS INFANTIS")
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.press("enter")
+    
+    #ABRINDO PRIMEIRO ARQUIVO
+    pyautogui.click(x=258, y=234, clicks=2)
+    pyautogui.click(x=2524, y=14)
+    time.sleep(6)
+
+    #EXCLUINDO BARRA DE BAIXO
+    pyautogui.press("f4")
+    pyautogui.click(x=1124, y=881, button='right')
+    pyautogui.click(x=1220, y=922)
+    pyautogui.press("2")
+    pyautogui.hotkey("ctrl", "s")
+    pyautogui.press("enter")
+    pyautogui.hotkey("alt", "f4")
+
+    #EXCLUINDO COPIA DE SEGURANÇA
+    
+    pyautogui.hotkey("win", "e")
+    pyautogui.click(x=540, y=101)
+    pyautogui.hotkey("ctrl", "a")
+    pyautogui.press("backspace")
+    pyperclip.copy(r"A:\videos\PACK 500 ESTAMPAS INFANTIS")
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.press("enter")
+    pyautogui.click(x=342, y=240)
+    pyautogui.press("del")
+    pyautogui.click(x=342, y=240)
+    pyautogui.hotkey("ctrl", "x")
+    pyautogui.click(x=230, y=212, clicks=2)
+    pyautogui.hotkey("ctrl", "v")
+    pyautogui.hotkey("alt", "f4")
